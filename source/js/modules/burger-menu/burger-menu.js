51fd409065c2ebb60cdc2550217c48e74ffbc7cf
Ирина <irene7494@mail.ru>
2023-05-18 01:49:24 +0300

    fix all the bugs

--- source/js/modules/burger-menu/burger-menu.js
+++ source/js/modules/burger-menu/burger-menu.js
@@ -1,10 +1,11 @@
 let header = document.querySelector('[data-navigation-status="header-navigation"]');
 let toggle = document.querySelector('[data-toggle-status="header-navigation-toggle"]');
 let links = document.querySelectorAll('[data-link-status="header-navigation-link"]');
+let closeOverlay = document.querySelector('[data-window-status="header-close-element"]');
 let body = document.querySelector('body');
 
 let initNav = function () {
-  if (header && toggle && links) {
+  if (header && toggle && links && window.innerWidth < 768) {
     header.classList.remove('nojs');
 
     let toggleMenu = function () {
@@ -12,9 +13,14 @@ let initNav = function () {
       header.classList.toggle('is-opened');
       header.classList.toggle('is-closed');
       body.style.overflow = isOpened ? 'visible' : 'hidden';
+      closeOverlay.style.display = isOpened ? 'none' : 'block';
     };
 
     toggle.addEventListener('click', function () {
+      toggleMenu();
+    });
+
+    closeOverlay.addEventListener('click', function () {
       toggleMenu();
     });
 
